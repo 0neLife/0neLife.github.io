2e35252b34470401d45b583fa5a51e3cdcc3b2cb
ybutsan <butsan.yuriy@gmail.com>
2020-10-08 23:00:35 +0300

    rewtired pwa config

--- pwa-demo/sw.js
+++ pwa-demo/sw.js
@@ -1,1 +1,47 @@
-var cacheName="hello-world-page",filesToCache=["./","./index.html","./css/main.min.css","./css/header.min.css","./js/registerWorker.min.js","./js/common.min.js"];self.addEventListener("install",function(e){console.log("[ServiceWorker] Install"),e.waitUntil(caches.open(cacheName).then(function(e){return console.log("[ServiceWorker] Caching app shell"),e.addAll(filesToCache)}))}),self.addEventListener("activate",function(e){e.waitUntil(self.clients.claim())}),self.addEventListener("fetch",function(n){n.respondWith(caches.match(n.request,{ignoreSearch:!0}).then(function(e){return e||fetch(n.request)}))});
+const staticCacheName = 'static-cache-v1.0';
+const dynamicCacheName = 'dynamic-cache-v1.0';
+const staticAssets = ['./', './index.html', './offline.html', './css/main.min.css', './css/header.min.css', './js/registerWorker.js', './js/common.js'];
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(staticAssets);
+  console.log('Service worker has been installed:');
+});
+self.addEventListener('activate', async event => {
+  const cachesKeys = await caches.keys();
+  const checkKeys = cachesKeys.map(async key => {
+    if (staticCacheName !== key) {
+      await caches.delete(key);
+    }
+  });
+  await Promise.all(checkKeys);
+  console.log('Service worker has been activated:');
+});
+self.addEventListener('fetch', async event => {
+  console.log('Trying to fetch:', event.request.url);
+  event.respondWith(checkCache(event.request));
+});
+
+async function checkCache(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || checkOnline(req);
+}
+
+async function checkOnline(req) {
+  const cache = await caches.open(dynamicCacheName);
+
+  try {
+    const res = await fetch(req);
+    await cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    const cachedRes = await cache.match(req);
+
+    if (cachedRes) {
+      return cachedRes;
+    } else if (req.url.includes('.html')) {
+      return caches.match('./offline.html');
+    } else {
+      console.log('return caches.match(./images/no-image.jpg)');
+    }
+  }
+}
