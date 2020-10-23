85aa5c6f1928b3c8557aaea7c0b6461af817aa92
ybutsan <butsan.yuriy@gmail.com>
2020-10-23 15:31:42 +0300

    add test push notif

--- pwa-bc-test/firebase-messaging-sw.js
+++ pwa-bc-test/firebase-messaging-sw.js
@@ -1,9 +1,10 @@
 importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js');
 var config = {
   apiKey: "AIzaSyDJENVW874nkGUYHqYpQoEZxAiMkdzpdSg",
   projectId: "pwa-bc-test",
+  authDomain: "pwa-bc-test.firebaseapp.com",
+  databaseURL: "https://pwa-bc-test.firebaseio.com",
   storageBucket: "pwa-bc-test.appspot.com",
   messagingSenderId: "664593019563",
   appId: "1:664593019563:web:80e7dfea6d28e20793bdee",
